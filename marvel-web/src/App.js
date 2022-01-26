8e761aecb050ec85b523fa10aca428867b07c25d
ana-225 <ana.vidal.s@hotmail.com>
2022-01-26 16:22:19 -0300

    Se agrega efecto flip a cartas

--- marvel-web/src/App.js
+++ marvel-web/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import axios from 'axios'
 import{useState, useEffect} from 'react'
+import Search from './componentes/Search'
 
 //https://gateway.marvel.com:443/v1/public/characters?apikey=c66aa45160257bef3f557f13a814ea58
 
@@ -15,7 +16,7 @@ import{useState, useEffect} from 'react'
 function App() {
 
   const[personajes, setPersonajes]=useState([])
-
+  // const[query, setQuery]= useState
   useEffect(()=> {
     axios.get('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=c66aa45160257bef3f557f13a814ea58&hash=61198adc6ab77ec06681a60a7603c719')
     .then(res=>{
@@ -29,20 +30,26 @@ function App() {
   
     <div className="App">
       <h1>Marvel</h1>
+    {/* <Search search={(q)=>setQuery(q)}></Search> */}
       
-      <div className="row row-cols-1 row-cols-md-2 g-4">
+      <div className="card-container row row-cols-1 row-cols-md-2 g-4">
      
      { personajes.map( per=>(
 
-       <div className="col" key={per.id}>
-      <div className="card" style={{width: "20rem" , height: "20rem"}}>
+       <div className="col card-container" key={per.id}>
+      <div className="card card-front" style={{width: "20rem" , height: "20rem"}}>
         <img src=  {`${per.thumbnail.path}.${per.thumbnail.extension}`}className="card-img-top" alt="..." style={{width: "80%" , height: "80%"}}/>
       <div className="card-body">
     <h5 className="card-title">{per.name}</h5>
-        <p className="card-text">{per.description}</p>
         {<p className="card-text">{per.modified}</p>}
         <a href="#" className="btn btn-primary">Go somewhere</a>
     </div>
+  </div>
+  <div className="card card-back" style={{width: "20rem" , height: "20rem"}}>
+        
+      <div className="card-body">
+        <p className="card-text">{per.description}</p>
+      </div>
   </div>
 </div>
 ))
